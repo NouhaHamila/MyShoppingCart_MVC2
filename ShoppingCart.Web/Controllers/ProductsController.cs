3e2979a917e80d79fa6152a296ad6f378b248923
Nouha Hamila <nouha.hamila@ixin.net>
2018-11-09 10:38:48 +0100

    Terminaison de la gestion du panier

--- ShoppingCart.Web/Controllers/ProductsController.cs
+++ ShoppingCart.Web/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShoppingCart.Core.Domain;
 using ShoppingCart.Core.Facade;
 namespace ShoppingCart.Web.Controllers
 {
@@ -18,6 +19,36 @@ namespace ShoppingCart.Web.Controllers
             return View(repo.FindAll());
         }
 
+        public ActionResult AddToCart(int id)
+        {
+            List<Item> panier =null;
+            Product produit = repo.FindAll().FirstOrDefault(p => p.ProductId.Equals(id));
+            //var panier = new List<Item>();
+            // var panier = Session["cart"];
+            if (Session["cart"] == null)
+            {
+                panier = new List<Item> {
+                    new Item {Product=produit , Quantity=1}
+                };
+                Session["cart"] = panier;
+            }
+            else
+            {
+                panier = (List<Item>)Session["cart"];
+                Item p = panier.Find(i => i.Product.ProductId.Equals(id));
+                if (p==null) //Le produit n'existe pas dans le panier
+                {
+                    panier.Add(new Item { Product=produit , Quantity=1 });
+                    Session["cart"] = panier;
+                }
+                else
+                {
+                    p.Quantity++;
+                    Session["cart"] = panier;
+                }
+            }
+            return View("Panier", panier);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
